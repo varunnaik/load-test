7a25fcc4b3df085260bdaf5a0543ac79d5af8045
Varun Naik <varun.naik@gmail.com>
2014-12-21 17:09:47 +0530

    Add test config area.

--- loadTestController.js
+++ loadTestController.js
@@ -1,3 +1,15 @@
 angular.module('loadTest', [])
-.controller('LoadTestController', ['$scope', function($scope) {
+.controller('LoadTestController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+        // For each provided URL, spin off a test thread with a timeout
+        // For each group of provided URLs, spin off n test threads with the given timeout
+        
+
+        $scope.getFrequency = 1;
+        $scope.numThreads = 2;
+        $scope.urlsToPing = "https://airfi.aero/\n" +
+        "https://airfi.aero/#/shop\n" +
+        "https://airfi.aero/#/games\n" +
+        "https://airfi.aero/#/crew\n" +
+        "https://airfi.aero/#/viewer";
+
     }]);
